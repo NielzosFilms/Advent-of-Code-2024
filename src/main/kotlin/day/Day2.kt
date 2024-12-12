068627dbfea9f095702c058714954dc9de02f1dd
Niels Hazelaar <nielzosfilms@gmail.com>
2024-12-12 22:59:50 +0100

    day2 part 1

--- src/main/kotlin/day/Day2.kt
+++ src/main/kotlin/day/Day2.kt
@@ -1,7 +1,54 @@
 package org.example.day
 
-class Day2 : DayHandler{
-    override fun process(input: List<String>) {
+import kotlin.math.abs
+import kotlin.math.min
 
+class Day2 : DayHandler {
+    override fun process(input: List<String>) {
+        val reports = this.convertToReports(input)
+
+        println(reports.filter { this.isSafeReport(it) }.size)
+    }
+
+    private fun convertToReports(input: List<String>): List<List<Int>> =
+        input.map { line -> line.split(" ").map { it.toInt() } }
+
+    private fun isSafeReport(report: List<Int>): Boolean {
+        val isIncreasing = this.isIncreasing(report)
+        var prevLevel = report[0]
+        for (i in 1 until report.size) {
+            val level = report[i]
+            if (this.isLevelBad(prevLevel, level, isIncreasing)) {
+                return false
+            }
+            prevLevel = level
+        }
+        return true
+    }
+
+    private fun isLevelBad(prevLevel: Int, level: Int, reportIsIncreasing: Boolean): Boolean {
+        if (prevLevel == level) return true
+        if (abs(prevLevel - level) !in 1..3) return true
+
+        if (reportIsIncreasing && level < prevLevel) return true
+        if (!reportIsIncreasing && level > prevLevel) return true
+
+        return false
+    }
+
+    private fun isIncreasing(report: List<Int>): Boolean {
+        var prevLevel = report[0]
+        var increasingCount = 0
+        var decreasingCount = 0
+        for (i in 1 until report.size) {
+            val level = report[i]
+            if (level > prevLevel) {
+                increasingCount++
+            } else {
+                decreasingCount++
+            }
+            prevLevel = level
+        }
+        return increasingCount > decreasingCount
     }
 }

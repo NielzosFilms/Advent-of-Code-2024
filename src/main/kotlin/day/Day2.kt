116f4c06ca508ce612c1f6b5c5c2e6c2a9d117b1
Niels Hazelaar <nielzosfilms@gmail.com>
2024-12-12 23:07:09 +0100

    day2 part 2

--- src/main/kotlin/day/Day2.kt
+++ src/main/kotlin/day/Day2.kt
@@ -1,17 +1,28 @@
 package org.example.day
 
 import kotlin.math.abs
-import kotlin.math.min
 
 class Day2 : DayHandler {
     override fun process(input: List<String>) {
         val reports = this.convertToReports(input)
 
         println(reports.filter { this.isSafeReport(it) }.size)
+
+        println(reports.filter { this.isSafeReportWithProblemDampener(it) }.size)
     }
 
     private fun convertToReports(input: List<String>): List<List<Int>> =
         input.map { line -> line.split(" ").map { it.toInt() } }
+
+    private fun isSafeReportWithProblemDampener(report: List<Int>): Boolean {
+        if (isSafeReport(report)) return true
+
+        for (i in report.indices) {
+            val slicedReport = report.subList(0, i) + report.subList(i + 1, report.size)
+            if (isSafeReport(slicedReport)) return true
+        }
+        return false
+    }
 
     private fun isSafeReport(report: List<Int>): Boolean {
         val isIncreasing = this.isIncreasing(report)
